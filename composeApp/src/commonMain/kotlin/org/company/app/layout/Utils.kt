f776fe70c52b11c373d0f58e7b9a6f6bb6bafc8c
dhiaspaner <78903066+dhiaspaner@users.noreply.github.com>
2024-04-19 14:27:23 +0100

    save

--- composeApp/src/commonMain/kotlin/org/company/app/layout/Utils.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/layout/Utils.kt
@@ -1,129 +1,16 @@
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
+package org.company.app.layout
+
 import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import org.company.app.panel.Panel
-import org.company.app.panel.PanelManager
 import org.company.app.panel.PanelSide
-import org.company.app.draggablePanel
 import org.company.app.toIntOffset
 import org.company.app.toSize
 import kotlin.math.roundToInt
 
-
-@Composable
-fun FourPanelsLayout(
-    panelManager: PanelManager
-) {
-    Layout(
-        content = {
-            Box(
-                modifier = Modifier
-                    .border(
-                        color = Color.Gray,
-                        width = 2.dp,
-                    )
-            ) {
-                Image(
-                    modifier = Modifier.fillMaxSize(),
-                    painter = painterResource("kotlin backround.png"),
-                    contentScale = ContentScale.FillBounds,
-                    contentDescription = "",
-                )
-
-
-            }
-            panelManager.panels.forEachIndexed { index, panelLayout ->
-
-                panelLayout.content(
-                    modifier = Modifier
-                        .draggablePanel(
-                            panel = panelLayout,
-                            onDragStart = { panel -> panelManager.onDragStart(panel) },
-                            onDrag = { panel -> panelManager.onDrag(panel) },
-                            onDragEnd = { panel -> panelManager.onDragEnd(panel) },
-                        )
-                )
-
-            }
-
-
-        },
-        modifier = Modifier
-    ) { measurables, constraints ->
-
-        val panelMeasurableList = measurables.slice(1 until panelManager.panels.size + 1)
-        val contentMeasurable = measurables.first()
-
-        val panelPlaceableList = panelManager.panels.mapIndexed { index, panel ->
-            customMeasure(
-                measurable = panelMeasurableList[index],
-                panel = panel,
-                constraints = constraints,
-            )
-        }
-
-        val topSidePlaceableList = panelPlaceableList.filter { it.panel.panelSide is PanelSide.Top }
-        val bottomSidePlaceableList = panelPlaceableList.filter { it.panel.panelSide is PanelSide.Bottom }
-        val leftSidePlaceableList = panelPlaceableList.filter { it.panel.panelSide is PanelSide.Left }
-        val rightSidePlaceableList = panelPlaceableList.filter { it.panel.panelSide is PanelSide.Right }
-        val freeSidePanelPlaceableList = panelPlaceableList.filter { it.panel.panelSide is PanelSide.Free }
-
-        val topPanelListHeightSum = topSidePlaceableList.sumOf { it.placeable.height }
-        val bottomPanelListHeightSum = bottomSidePlaceableList.sumOf { it.placeable.height }
-        val leftPanelListWidthSum = leftSidePlaceableList.sumOf { it.placeable.width }
-        val rightPanelListWidthSum = rightSidePlaceableList.sumOf { it.placeable.width }
-
-        panelManager.topSplitter = topPanelListHeightSum
-        panelManager.bottomSplitter = constraints.maxHeight - bottomPanelListHeightSum
-        panelManager.leftSplitter = leftPanelListWidthSum
-        panelManager.rightSplitter = constraints.maxWidth - rightPanelListWidthSum
-
-
-        val contentPlaceable = contentMeasurable.measure(
-            constraints.copy(
-                maxWidth = constraints.maxWidth -
-                        rightPanelListWidthSum -
-                        leftPanelListWidthSum,
-                maxHeight = constraints.maxHeight -
-                        topPanelListHeightSum +
-                        bottomPanelListHeightSum
-            )
-        )
-
-        panelManager.maxWidth = constraints.maxWidth
-        panelManager.maxHeight = constraints.maxHeight
-
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            contentPlaceable.place(
-                x = leftSidePlaceableList.sumOf { it.placeable.width },
-                y = topSidePlaceableList.sumOf { it.placeable.height }
-            )
-
-            placeLeftSidePanelList(leftSidePlaceableList, topPanelListHeightSum)
-            placeRightSidePanelList(rightSidePlaceableList, constraints.maxWidth, topPanelListHeightSum)
-            placeTopSidePanelList(topSidePlaceableList)
-            placeBottomSidePanelList(bottomSidePlaceableList, constraints.maxHeight)
-            placeFreeSidePanelPlaceableList(freeSidePanelPlaceableList)
-
-        }
-
-    }
-}
 
 data class PanelPlaceable(
     val panel: Panel,
@@ -173,7 +60,6 @@ fun MeasureScope.customMeasure(
         }
 
         is PanelSide.Bottom -> {
-            println("botto, placeable")
             PanelPlaceable(
                 panel = panel,
                 placeable = measurable.measure(
@@ -253,5 +139,3 @@ fun Placeable.PlacementScope.placeFreeSidePanelPlaceableList(panelPlaceableList:
         panelPlaceable.placeable.place(offset)
     }
 }
-
-
--- composeApp/src/commonMain/kotlin/org/company/app/panel/SidePanels.kt
+++
@@ -1,8 +0,0 @@
-package org.company.app.panel
-
-data class SidePanels (
-    val topSidePanel: Panel,
-    val bottomSidePanel: Panel,
-    val leftSidePanel: Panel,
-    val rightSidePanel: Panel
-)
